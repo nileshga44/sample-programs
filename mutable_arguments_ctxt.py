47091632a5b20f62585488545aa049ee8e1f0ca4
Hansraj Das <raj.das.136@gmail.com>
2018-04-26 21:37:42 +0530

    Mutable optional arguments

---
+++ mutable_arguments_ctxt.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+# Date: 2017-mm-dd
+#
+# Description: When a mutable parameter is optional and value is not passed
+#              while calling, it retains previous value. 
+
+def f(x, l=[]):
+  for i in range(x):
+    l.append(i*i)
+  print(l) 
+
+f(2)
+f(3, [3, 2, 1])
+f(3)  # l updated with first call f(2) .i.e. [0, 1] is maintained in this call.
+f(4)
+
+# Output:
+# [0, 1]
+# [3, 2, 1, 0, 1, 4]
+# [0, 1, 0, 1, 4]
+# [0, 1, 0, 1, 4, 0, 1, 4, 9]
