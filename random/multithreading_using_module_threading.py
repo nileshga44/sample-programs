0971eae411a1f2ff60ec83bbbec24634a1d37f0d
Hansraj Das <raj.das.136@gmail.com>
2018-09-03 22:05:27 +0530

    threading files combined

--- random/multithreading_using_module_threading.py
+++ random/multithreading_using_module_threading.py
@@ -1,5 +1,5 @@
-from threading import Thread #Thread is build-in python class for multithreading
 import time
+import threading
 
 def sleeper(i):
     print ("\nThread %d sleeps for 5 seconds" % (i))
@@ -7,5 +7,35 @@ def sleeper(i):
     print ("\nThread %d woke up" % (i))
 
 for i in range (10):
-    thr = Thread(target=sleeper, args=(i,))
+    thr = threading.Thread(target=sleeper, args=(i,))
     thr.start()
+
+
+# Another example.
+class primeNumber(threading.Thread):
+    def __init__(self, number):
+        threading.Thread.__init__(self)
+        self.Number = number
+    
+    def run(self):
+        counter = 2
+        while (counter < self.Number/2):
+            if (0 == self.Number % counter):
+                print ("\nNumber [%d] is not prime" % (self.Number))
+                return 
+            else:
+                counter += 1
+        else:
+            print ("\nNumber [%d] is prime" % (self.Number))
+
+threads = []
+while True:
+    num = long(input("\nEnter number: "))
+    if (1 > num):
+        break
+    thread = primeNumber(num)
+    threads.append(thread)
+    thread.start()
+
+for x in threads:
+    x.join()
