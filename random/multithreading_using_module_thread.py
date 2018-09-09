a8d5611920948f8c6fbe67d720fc7573820a0d67
Hansraj Das <raj.das.136@gmail.com>
2018-09-09 23:38:29 +0530

    threading revisited

--- random/multithreading_using_module_thread.py
+++ random/multithreading_using_module_thread.py
@@ -1,24 +1,30 @@
-import thread #This module is not present in python 3 and above, instead module "threading" is available
+# thread module is not present in python 3 and above, instead module
+# "threading" is available.
+import thread
 import time
-print ("\n--------------------------Multithreading without lock mechanism------------------------------------")
+
+print("Multithreading without lock mechanism")
 num_threads = 0
 def thread_function(thrId, a):
     global num_threads
-    print ("\n 1 | thrId [%s] | num_threads[%d]" % (thrId, num_threads))
+    print("\n1 | thrId[%d] | num_threads[%d]" % (thrId, num_threads))
     num_threads += 1
-    print ("\n 2 | thrId [%s] | num_threads[%d]" % (thrId, num_threads))
+    print("\n2 | thrId[%d] | num_threads[%d]" % (thrId, num_threads))
     num_threads -= 1
-    print ("\n 3 | thrId [%s] | num_threads[%d]" % (thrId, num_threads))
+    print("\n3 | thrId[%d] | num_threads[%d]" % (thrId, num_threads))
 
 thread.start_new_thread(thread_function, (1, 0))
 thread.start_new_thread(thread_function, (2, 0))
 thread.start_new_thread(thread_function, (3, 0))
-print ("\nExiting...")
+print("\nExiting...")
 
-#Problem with above code - num_threads value is not synchronized because simultaneous read and write is taking without any lock.
-#To cater above problem we will use locking mechanism to access critical data/section
+# Problem with above code - num_threads value is not synchronized because
+# simultaneous read and write is taking without any lock.
+# To cater above problem we will use locking mechanism to access critical
+# data/section.
+
 time.sleep(1)
-print ("\n--------------------------Multithreading with lock mechanism------------------------------------")
+print("\n\nMultithreading with lock mechanism")
 
 num_threads1 = 0
 thread_started = False
@@ -26,18 +32,18 @@ lock = thread.allocate_lock()
 def thread_function1(thrId, a):
     global num_threads1
     lock.acquire()
-    print ("\n 1 | thrId [%s] | num_threads1[%d]" % (thrId, num_threads1))
+    print("\n1 | thrId[%d] | num_threads1[%d]" % (thrId, num_threads1))
     num_threads1 += 1
     thread_started = True
-    print ("\n 2 | thrId [%s] | num_threads1[%d]" % (thrId, num_threads1))
+    print("\n2 | thrId[%d] | num_threads1[%d]" % (thrId, num_threads1))
     lock.release()
     lock.acquire()
     num_threads1 -= 1
-    print ("\n 3 | thrId [%s] | num_threads1[%d]" % (thrId, num_threads1))
+    print("\n3 | thrId[%d] | num_threads1[%d]" % (thrId, num_threads1))
     lock.release()
 
 thread.start_new_thread(thread_function1, (1, 0))
 thread.start_new_thread(thread_function1, (2, 0))
 thread.start_new_thread(thread_function1, (3, 0))
-print ("\nExiting...")
+print("\nExiting...")
 time.sleep(1)
--- random/multithreading_using_module_threading.py
+++ random/multithreading_using_module_threading.py
@@ -2,36 +2,41 @@ import time
 import threading
 
 def sleeper(i):
-    print ("\nThread %d sleeps for 5 seconds" % (i))
+    print('\nThread %d sleeps for 5 seconds' % i)
     time.sleep(5)
-    print ("\nThread %d woke up" % (i))
+    print('\nThread %d woke up' % i)
 
-for i in range (10):
+for i in range(10):
     thr = threading.Thread(target=sleeper, args=(i,))
     thr.start()
 
 
-# Another example.
+time.sleep(7)
+print '\n\nAnother example of threading using class(prime checker)...'
+time.sleep(1)
+
 class primeNumber(threading.Thread):
     def __init__(self, number):
         threading.Thread.__init__(self)
         self.Number = number
     
+    # Implement run method in threading.Thread class, this will be called when
+    # we do thread.start().
     def run(self):
         counter = 2
-        while (counter < self.Number/2):
-            if (0 == self.Number % counter):
-                print ("\nNumber [%d] is not prime" % (self.Number))
+        while counter < self.Number/2:
+            if not self.Number % counter:
+                print('\nNumber[%d] is not prime' % self.Number)
                 return 
             else:
                 counter += 1
         else:
-            print ("\nNumber [%d] is prime" % (self.Number))
+            print('\nNumber[%d] is prime' % self.Number)
 
 threads = []
 while True:
-    num = long(input("\nEnter number: "))
-    if (1 > num):
+    num = long(input('\nEnter number(0 to exit): '))
+    if 1 > num:
         break
     thread = primeNumber(num)
     threads.append(thread)
