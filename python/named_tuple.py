19589d46bc981a138e3027b920b2e07be9b8acb4
nishaagrawal16 <51795688+nishaagrawal16@users.noreply.github.com>
2020-10-14 22:19:36 +0530

    Python3 compatible - Update print

--- python/named_tuple.py
+++ python/named_tuple.py
@@ -11,18 +11,18 @@ Records = []
 Record = collections.namedtuple('Record', ['Id', 'Name', 'Age'])
 
 record = Record(1, 'Abc', 10)
-print record
+print(record)
 # Record(Id=1, Name='Abc', Age=10)
 
 Records.append(record)
 Records.append(Record(2, 'Xyz', 20))
-print Records  
+print(Records)
 # [Record(Id=1, Name='Abc', Age=10), Record(Id=2, Name='Xyz', Age=20)]
 
 d = Records[0]._asdict()
-print d
+print(d)
 # OrderedDict([('Id', 1), ('Name', 'Abc'), ('Age', 10)])
 
 d = {'Id': 10, 'Name': 'pppp', 'Age': 22}
-print Record(**d)
+print(Record(**d))
 # Record(Id=10, Name='pppp', Age=22)
